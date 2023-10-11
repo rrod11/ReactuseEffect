511b5075b579ddfceb065070cc86ebbb4bce762c
Roderick <Roderickj800@gmail.com>
2023-10-11 18:15:02 -0400

    pictures done

--- src/components/PictureDisplay.js
+++ src/components/PictureDisplay.js
@@ -9,6 +9,7 @@ import feather7 from "../images/feather7.svg";
 import feather8 from "../images/feather8.svg";
 import feather9 from "../images/feather9.svg";
 import featherA from "../images/featherA.svg";
+import { useEffect, useState } from "react";
 
 const feathers = [
   feather1,
@@ -23,26 +24,51 @@ const feathers = [
   featherA,
 ];
 
-function PictureDisplay ({ size, featherCount, featherColors }) {
-  console.log('PictureDisplay', size, featherCount, featherColors);
+function PictureDisplay({ size, featherCount, featherColors }) {
+  useEffect(() => {
+    console.log("PictureDisplay size", size);
+    let sizing = "";
+    switch (size) {
+      case "m":
+        sizing = "medium";
+        break;
+      case "l":
+        sizing = "large";
+        break;
+      case "xl":
+        sizing = "xlarge";
+        break;
+      default:
+        sizing = "small";
+        break;
+    }
+    setSizeClass(sizing);
+  }, [size]);
+  const [sizeClass, setSizeClass] = useState("");
+  useEffect(() => {
+    console.log("PictureDisplay featherCount", featherCount);
+  }, [featherCount]);
+  useEffect(() => {
+    console.log("PictureDisplay featherColors", featherColors);
+  }, [featherColors]);
 
   // TODO: Wrap in useEffect
   const colors = [];
-  if (!featherColors || featherColors.length === 0) featherColors = [''];
-  for (let i=0; i<featherCount; i++) {
+  if (!featherColors || featherColors.length === 0) featherColors = [""];
+  for (let i = 0; i < featherCount; i++) {
     colors.push(featherColors[i % featherColors.length]);
   }
 
   return (
-    <div className={`image-area medium`}>
-      {colors.map((c, i) =>
-        <img  
-          key={feathers[i]} 
-          src={feathers[i]} 
-          className={`image-feather ${c}`} 
-          alt="" 
+    <div className={`image-area ${sizeClass}`}>
+      {colors.map((c, i) => (
+        <img
+          key={feathers[i]}
+          src={feathers[i]}
+          className={`image-feather ${c}`}
+          alt=""
         />
-      )}
+      ))}
 
       <img src={turkey} className="image-turkey" alt="turkey" />
     </div>
