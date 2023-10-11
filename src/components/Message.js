be4cb394dfd1b90ba698abdff3034cc580cfe883
Roderick <Roderickj800@gmail.com>
2023-10-11 18:14:45 -0400

    messages done

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,38 @@
-function Message({ size }) {
-  console.log('Message', size);
+import { useEffect, useState } from "react";
 
-  return (
-    <div className="message medium">
-      (Oh my! Your bird is naked!)
-    </div>
-  );
-};
+function Message({ size, featherCount }) {
+  useEffect(() => {
+    console.log("Message", size);
+    let sizing = "";
+    switch (size) {
+      case "m":
+        sizing = "medium";
+        break;
+      case "l":
+        sizing = "large";
+        break;
+      case "xl":
+        sizing = "xlarge";
+        break;
+      default:
+        sizing = "small";
+        break;
+    }
+    setSizeClass(sizing);
+  }, [size]);
+  useEffect(() => {
+    if (!featherCount) {
+      setMessage("Oh Noooo. Where are your clothes??");
+    } else if (featherCount == 10) {
+      setMessage("Thats a well dressed CHICKEN");
+    } else {
+      setMessage("At least your duck is wearing something");
+    }
+  }, [featherCount]);
+  const [sizeClass, setSizeClass] = useState("");
+  const [message, setMessage] = useState("");
+
+  return <div className={`message ${sizeClass}`}>{message}</div>;
+}
 
 export default Message;
